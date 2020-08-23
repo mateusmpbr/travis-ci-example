778cfa1719ecb286eb330fec3349eee478bed85d
Travis CI <mateusmartinspereira@gmail.com>
2020-08-23 02:55:41 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.940c8b7d7aaccede7850c41af452c0e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c697c6c844e1d65d18a8578947f326e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "87429ac22919be2b997a",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "732d647e91eb5ff0649b",
+    "url": "./static/js/2.6e3685a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6e3685a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87429ac22919be2b997a",
+    "url": "./static/js/main.666f8f94.chunk.js"
+  },
+  {
+    "revision": "7db0b016525addd2f34b",
+    "url": "./static/js/runtime-main.0c160550.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
